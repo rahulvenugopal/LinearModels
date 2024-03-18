a6117f03c8452a05c5a5a4d46d9de03ae77aa59c
Rahul Venugopal <rhlvenugopal@gmail.com>
2024-03-18 16:45:36 +0530

    Update 01_simple_regression.R

--- scripts/01_simple_regression.R
+++ scripts/01_simple_regression.R
@@ -26,28 +26,28 @@ str(data)
 # Let us start with visualization
 data$species <- as.factor(data$species)
 
-# Set up the theme
-theme_update(
-  axis.ticks = element_line(color = "grey92"),
-  axis.ticks.length = unit(1, "lines"),
-  panel.grid.minor = element_blank(),
-  legend.title = element_text(size = 20),
-  legend.text = element_text(color = "grey30"),
-  axis.text.x = element_text(color = "grey20", size = 20),
-  axis.text.y = element_text(color = "grey20", size = 20),
-  plot.title = element_text(size = 18, face = "bold"),
-  plot.subtitle = element_text(size = 12, color = "grey30"),
-  plot.caption = element_text(size = 9, margin = margin(t = 15))
-) + 
-  theme(plot.margin = margin(t = 25, r = 25, b = 10, l = 25)) 
-
 # Body mass predicted by species ------------------------------------------
 
 p1 <- ggplot(data,
              aes(x = species, y = body_mass_g, color = species)) +
   geom_boxplot(alpha = 0.6, width = 0.5) +
   geom_jitter(width = 0.1) +
-  labs(x = "Species", y = "Body mass")
+  labs(x = "Species", y = "Body mass") + 
+  # Set up the theme
+  theme(axis.ticks = element_line(color = "grey92"),
+        axis.ticks.length = unit(1, "lines"),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size = 18),
+        axis.title.x = element_blank(),
+        legend.text = element_text(color = "grey30", size = 18),
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(color = "grey20", size = 10),
+        plot.title = element_text(size = 30, face = "bold"),
+        plot.subtitle = element_text(size = 12, color = "grey30"),
+        plot.caption = element_text(size = 9, margin = margin(t = 15)),
+        legend.position = "bottom",
+        legend.title = element_blank(),
+        plot.margin = margin(t = 25, r = 25, b = 10, l = 25)) 
 
 bodymass_species <- lm(body_mass_g ~ species,
                        data = data)
